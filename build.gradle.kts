cc464bb22f7b79f857f33cdb52c54ef3ddb4c12c
Martijn Muijsers <martijnmuijsers@live.nl>
2024-01-27 23:47:37 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.5"
+    id("io.papermc.paperweight.patcher") version "1.5.11"
     id("com.github.ManifestClasspath") version "0.1.0-RELEASE"
 }
 
@@ -26,7 +26,7 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
     decompiler("net.minecraftforge:forgeflower:2.0.627.2")
     paperclip("io.papermc:paperclip:3.0.3")
 }
