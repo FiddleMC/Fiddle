04513ed35484eac85662343831c7d9269fcad337
Martijn Muijsers <martijnmuijsers@live.nl>
2026-02-05 12:34:54 +0100

    Update vanilla flag packages

--- fiddle-api/src/main/java/org/fiddlemc/fiddle/api/moredatadriven/vanillaflag/package-info.java
+++ fiddle-api/src/main/java/org/fiddlemc/fiddle/api/moredatadriven/vanillaflag/package-info.java
@@ -1,17 +1,8 @@
 /**
- * <h1>Module: More data-driven - Vanilla flag - API</h1>
+ * <h1>More data-driven - Vanilla flag - API part</h1>
  *
  * <p>
  * Adds API to see which blocks and items are vanilla.
- * </p>
- *
- * <p>
- * <h3>Paper changes</h3>
- * <ul>
- *     <li>{@link org.bukkit.block.BlockType} - To add method that checks if a block type is vanilla.</li>
- *     <li>{@link org.bukkit.block.data.BlockData} - To add method that checks if a block state is vanilla.</li>
- *     <li>{@link org.bukkit.inventory.ItemType} - To add method that checks if an item type is vanilla.</li>
- * </ul>
  * </p>
  */
 @NullMarked
--- fiddle-server/src/main/java/org/fiddlemc/fiddle/impl/moredatadriven/vanillaflag/package-info.java
+++ fiddle-server/src/main/java/org/fiddlemc/fiddle/impl/moredatadriven/vanillaflag/package-info.java
@@ -1,34 +1,8 @@
 /**
- * <h1>Module: More data-driven - Vanilla flag</h1>
+ * <h1>More data-driven - Vanilla flag - Implementation part</h1>
  *
  * <p>
- * Adds a flag to blocks and items, indicating whether they are vanilla or not.
- * </p>
- *
- * <p>
- * <h3>Paper changes</h3>
- * <ul>
- *     <li>
- *         {@link org.bukkit.craftbukkit.block.CraftBlockType},
- *         {@link org.bukkit.craftbukkit.block.data.CraftBlockData} and
- *         {@link org.bukkit.craftbukkit.block.CraftBlockType} - To add a method for getting the flag.
- *     </li>
- * </ul>
- * </p>
- *
- * <p>
- * <h3>Minecraft changes</h3>
- * <ul>
- *     <li>
- *         {@link net.minecraft.world.level.block.Block}
- *         {@link net.minecraft.world.level.block.state.BlockState} and
- *         {@link net.minecraft.world.item.Item} - To add the flag as a field.
- *     </li>
- *     <li>
- *         {@link net.minecraft.world.level.block.Blocks} and
- *         {@link net.minecraft.world.item.Items} - To set the flag.
- *     </li>
- * </ul>
+ * Implementation of {@link org.fiddlemc.fiddle.api.moredatadriven.vanillaflag}.
  * </p>
  */
 @NullMarked
